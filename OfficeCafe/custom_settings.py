667cee8b397a33878640c389b0d62a95662574da
ag93999 <adityagupta93999@gmail.com>
2018-08-01 18:33:31 +0530

    Removed pagination

--- OfficeCafe/custom_settings.py
+++ OfficeCafe/custom_settings.py
@@ -24,7 +24,7 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'PAGE_SIZE': 10,
 
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
