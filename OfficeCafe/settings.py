25626f64666c4ef137870bed13be7700083c6c3d
Himanshu Shankar <me@himanshus.com>
2018-12-27 19:33:39 +0530

    Override method middleware

--- OfficeCafe/settings.py
+++ OfficeCafe/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsPostCsrfMiddleware',
+    'order.utils.MethodOverrideMiddleware',
 ]
 
 ROOT_URLCONF = 'OfficeCafe.urls'
