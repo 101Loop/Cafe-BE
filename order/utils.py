931560afd33bb55ee6fd9fdf806fa226cbea9104
Himanshu Shankar <me@himanshus.com>
2018-12-27 19:44:47 +0530

    Fixed Override method middleware

--- order/utils.py
+++ order/utils.py
@@ -1,9 +1,24 @@
 METHOD_OVERRIDE_HEADER = 'HTTP_X_HTTP_METHOD_OVERRIDE'
 
 
-class MethodOverrideMiddleware(object):
+class MethodOverrideMiddleware:
 
-    def process_view(self, request, callback, callback_args, callback_kwargs):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+    def process_view(self, request):
         if request.method != 'POST':
             return
         if METHOD_OVERRIDE_HEADER not in request.META:
